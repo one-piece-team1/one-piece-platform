cfb5b49d176de8005d3260134b40bce2bd29f705
mina.h <mina.h@amaryllo.us>
2021-02-13 16:45:43 +0800

    <finished><bottomSheet><bottomSheet with expansion pane list>

--- lib/ui/components/common/default_scaffold.dart
+++ lib/ui/components/common/default_scaffold.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:one_piece_platform/core/models/entry_model.dart';
 import 'package:one_piece_platform/ui/components/buttons/flat_icon_button.dart';
 import 'package:one_piece_platform/ui/components/common/ticket.dart';
 import 'package:one_piece_platform/ui/components/image/thumbnail.dart';
+import 'package:one_piece_platform/ui/components/expansion/entry_item.dart';
 import 'package:one_piece_platform/ui/screens/user/user_dynamic_header.dart';
 
 import '../../constants.dart';
@@ -15,6 +17,34 @@ class DefaultScaffold extends StatelessWidget {
   });
 
   final bool showSpinner;
+
+// The entire multilevel list displayed by this app.
+  final List<Entry> data = <Entry>[
+    Entry(
+      'Company',
+      <Entry>[
+        Entry('Matcha Inc.'),
+        Entry('Roasted green tea Inc.'),
+        Entry('A2'),
+      ],
+    ),
+    Entry(
+      'Categories',
+      <Entry>[
+        Entry('B0'),
+        Entry('B1'),
+        Entry('B2'),
+      ],
+    ),
+    Entry(
+      'Port',
+      <Entry>[
+        Entry('C0'),
+        Entry('C1'),
+        Entry('C2'),
+      ],
+    ),
+  ];
 
 //  final List<Widget> contents;
   @override
@@ -31,7 +61,53 @@ class DefaultScaffold extends StatelessWidget {
             Expanded(
               flex: 1,
               child: FlatIconButton(
-                onPressed: () {},
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Row(
+                            // A Row widget
+                            mainAxisAlignment: MainAxisAlignment
+                                .spaceBetween, // Free space will be equally divided and will be placed between the children.
+                            children: <Widget>[
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                // A Flexible widget that will make its child flexible
+                                child: Text(
+                                  "Filter",
+                                  overflow: TextOverflow
+                                      .ellipsis, // handles overflowing of text
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.clear), // some random icon
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ), //
+                            ],
+                          ),
+                          Expanded(
+                            child: ListView.builder(
+                              itemBuilder: (BuildContext context, int index) {
+                                return EntryItem(data[index]);
+                              },
+                              itemCount: data.length,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
                 icon: Icons.filter_alt,
                 labelText: 'Filter',
                 iconColor: kIconPrimaryColor,
@@ -49,7 +125,7 @@ class DefaultScaffold extends StatelessWidget {
               flex: 1,
               child: FlatIconButton(
                 onPressed: () {},
-                icon: Icons.sort,
+                icon: Icons.swap_vert,
                 labelText: 'Sort',
                 iconColor: kIconPrimaryColor,
                 textColor: kIconPrimaryColor,
