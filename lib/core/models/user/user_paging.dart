5c955d629c0e8200406e41c99f4178b579382d91
mina.h <mina.h@amaryllo.us>
2021-03-12 17:18:38 +0800

    <add><models><locations_paging>

--- lib/core/models/user/user_paging.dart
+++ lib/core/models/user/user_paging.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:one_piece_platform/core/models/user/user_by_id_model.dart';
 
 class UserPaging {
-  UserById user;
+  List<UserById> user;
   int take;
   int skip;
   int count;
@@ -15,10 +15,10 @@ class UserPaging {
   });
 
   factory UserPaging.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['users'] as List;
+    List<UserById> userList = list.map((i) => UserById.fromJson(i)).toList();
     return UserPaging(
-      user: UserById.fromJson(
-        parsedJson['users'],
-      ),
+      user: userList,
       take: parsedJson['take'],
       skip: parsedJson['skip'],
       count: parsedJson['count'],
